d28cc22e54b4cf1b7690990312131bd5fb9332ec
EdwinNino-24 <edwinnino2404@gmail.com>
2025-05-15 22:33:44 -0500

    RAAA

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "dist/secure-auth-frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/secure-auth-frontend/index.html"));
+  res.sendFile(path.join(__dirname, "dist/secure-auth-frontend/browser/login/index.html"));
 });
 
 app.listen(port, () => {
